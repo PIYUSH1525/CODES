1afd34ead7166c4214e48b2c2c38047f54cab8a5
Piyush Negi <negipiyush125@gmail.com>
2024-08-16 23:36:33 +0530

    Majority Elememt approach 3

--- Majority Element.py
+++ Majority Element.py
@@ -2,6 +2,24 @@ from typing import List
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
+        # APPROACH 3
+        "USING VOTING ALGO"
+        Vote = 1
+        majority_element = nums[0]
+        for i in range(1,len(nums)):
+            if Vote == 0:
+                majority_element = nums[i]
+                Vote += 1
+            elif majority_element == nums[i]:
+                Vote +=1
+            else:
+                Vote -=1
+        return majority_element
+
+
+
+
+
         # APPROACH 2
         "Using Dictionary Key value pair"
         # l = len(nums)
@@ -21,14 +39,6 @@ class Solution:
         # return nums[len(nums)//2]
 
 
-
-
-
-
-
-
-
-
-nums = [3,3,4]
+nums = [3,4,3,4,3]
 piyush =Solution()
 print(piyush.majorityElement(nums))
