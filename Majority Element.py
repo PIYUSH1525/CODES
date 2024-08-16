b946bdaa7dc371f75149aa547d617d77ea77819c
Piyush Negi <negipiyush125@gmail.com>
2024-08-16 15:54:59 +0530

    Majority Elememt approach 2

--- Majority Element.py
+++ Majority Element.py
@@ -1,8 +1,29 @@
 from typing import List
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+
+        # APPROACH 2
+        "Using Dictionary Key value pair"
+        # l = len(nums)
+        # dicto = {}
+        # for num in nums:
+        #     if num not in dicto:
+        #         dicto[num] = 1
+        #     else:
+        #         dicto[num] += 1
+        #     if dicto[num] > l // 2:
+        #         return num
+
+
+        # APPROACH 1
+        "the majority element will always come in center due to the condition n/2 so sort the array and return"
+        "the center element "
+        # nums.sort()
+        # return nums[len(nums)//2]
+
+
+
+
 
 
 
