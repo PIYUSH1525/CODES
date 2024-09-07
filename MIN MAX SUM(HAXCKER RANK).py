40079a3bc3d1c9bdecffec70b6d088414f3e3001
Piyush Negi <negipiyush125@gmail.com>
2024-09-07 21:30:04 +0530

    CODE

--- MIN MAX SUM(HAXCKER RANK).py
+++ MIN MAX SUM(HAXCKER RANK).py
@@ -1,5 +1,18 @@
-Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+#
+# Example
+#
+# The minimum sum is  and the maximum sum is . The function prints
+#!/bin/python3
 
-Example
+import math
+import os
+import random
+import re
+import sys
 
-The minimum sum is  and the maximum sum is . The function prints
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
