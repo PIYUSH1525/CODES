234ffe52ffab4fccf90b5747a6c69f5d892cbf20
Piyush Negi <negipiyush125@gmail.com>
2024-09-07 22:58:45 +0530

    CODE

--- HOUR CONVERSON.py
+++ HOUR CONVERSON.py
@@ -10,3 +10,42 @@
 #
 #
 # Return '00:01:00'.
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    if s[-2:] == 'AM':
+        if s[:2] == '12':
+            return "00" + s[2:-2]
+        else:
+            return s[:-2]
+    else:
+        h = int(s[:2])
+        if h < 12:
+            h += 12
+        return str(h) + s[2:-2]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
