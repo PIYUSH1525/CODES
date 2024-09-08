ac24d4d3669f5c6ace412dc7d863c2018f5da781
Piyush Negi <negipiyush125@gmail.com>
2024-09-08 23:24:18 +0530

    CODE

--- leetcode 977.py
+++ leetcode 977.py
@@ -42,3 +42,18 @@ class Solution:
             # nums[i].append(d)
         nums.sort()
         return nums
+# APPROACH 2
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res = []
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] * nums[l] > nums[r] * nums[r]:
+                res.append(nums[l] * nums[l])
+                l += 1
+            else:
+                res.append(nums[r] * nums[r])
+                r -= 1
+
+        return res[::- 1]  # reverse
