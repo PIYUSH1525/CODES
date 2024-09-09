0e2fd918568f28b8930a21c22366624399671b48
Piyush Negi <negipiyush125@gmail.com>
2024-09-09 22:16:54 +0530

    CODE

---
+++ ENCODE AND DECODE StRING.py
@@ -0,0 +1,10 @@
+# String Encode and Decode
+# Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+#
+# Please implement encode and decode
+# MEDIUN
+# Example 1:
+#
+# Input: ["neet","code","love","you"]
+#
+# Output:["neet","code","love","you"]
