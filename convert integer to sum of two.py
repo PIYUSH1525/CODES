3ba45a83374edb2513d96eff4bf93f73ff52ea33
Piyush Negi <negipiyush125@gmail.com>
2024-09-10 23:05:05 +0530

    CODE

--- convert integer to sum of two.py
+++ convert integer to sum of two.py
@@ -99,3 +99,8 @@
 # be
 # accepted.
 # #
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        for i in range(n):
+            if "0" not in f"{i}{n - i}":
+                return [i, n - i]
