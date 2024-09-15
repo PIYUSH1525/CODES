cdfd64e8c6636e3f2a9b67dc45c07a025a90ac4c
Piyush Negi <negipiyush125@gmail.com>
2024-09-15 12:22:05 +0530

    CODE

---
+++ HACKERRANK ARR REV.py
@@ -0,0 +1,8 @@
+# An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  or .
+#
+# Reverse an array of integers.
+#
+# Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this.
+#
+# Example
+#
