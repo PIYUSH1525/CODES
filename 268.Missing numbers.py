eab0a134320396663210c410a47845a6fef75cd5
Piyush Negi <negipiyush125@gmail.com>
2024-09-15 22:52:59 +0530

    CODE

--- 268.Missing numbers.py
+++ 268.Missing numbers.py
@@ -5,3 +5,10 @@
 # 2 numbers, so all numbers are in the range[0, 2]. 2 is the missing number in the range since it does not appear in
 # nums. Example 3:# Input: nums = [9, 6, 4, 2, 3, 5, 7, 0, 1] Output: 8 Explanation: n = 9 since there are 9 numbers, so
 # all numbers are in the range[0, 9]. 8 is the missing number in the range since it does not appear in nums.
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
