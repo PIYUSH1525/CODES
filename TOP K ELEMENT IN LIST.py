5fbe20b5492b89e14312e5e0217a1e659b0cfb0f
Piyush Negi <negipiyush125@gmail.com>
2024-09-15 21:49:06 +0530

    CODE

--- TOP K ELEMENT IN LIST.py
+++ TOP K ELEMENT IN LIST.py
@@ -10,3 +10,14 @@
 # Input: nums = [1,2,2,3,3,3], k = 2
 #
 # # Output: [2,3]
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashset = {}
+        for i in nums:
+            if i in hashset:
+                hashset[i] += 1
+            else:
+                hashset[i] = 1
+        for i in range(k):
+            for num, count in hashset.items():
+                return num
