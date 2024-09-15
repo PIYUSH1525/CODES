246fdd920ada9e7cf4a91cfdd5a7e4591e4fafdb
Piyush Negi <negipiyush125@gmail.com>
2024-09-15 12:04:13 +0530

    CODE

--- Hackerrank Leaop yaeart.py
+++ Hackerrank Leaop yaeart.py
@@ -12,3 +12,17 @@
 # Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
 #
 # Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
+def is_leap(year):
+    leap = False
+
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+year = int(input())
