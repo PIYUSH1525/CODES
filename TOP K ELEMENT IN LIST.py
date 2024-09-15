483b84a93b326f4a1b8c5e2b0ef0a3b7dc8ee3f0
Piyush Negi <negipiyush125@gmail.com>
2024-09-15 12:31:18 +0530

    CODE

---
+++ TOP K ELEMENT IN LIST.py
@@ -0,0 +1,12 @@
+# Top K Elements in List
+# Given an integer array nums and an integer k, return the k most frequent elements within the array.
+#
+# The test cases are generated such that the answer is always unique.
+#
+# You may return the output in any order.
+#
+# Example 1:
+#
+# Input: nums = [1,2,2,3,3,3], k = 2
+#
+# # Output: [2,3]
